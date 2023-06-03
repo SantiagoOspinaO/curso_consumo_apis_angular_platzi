90265e9d2524e42fa36a01d9beb800f7a0b6effe
SantiagoOspinaO <santiago.m200@outlook.es>
2023-06-03 00:38:40 -0500

    Curso de consumo de APIS Rest con Angular

---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,112 @@
+import { Component, OnInit } from '@angular/core';
+
+import { CreateProductDTO, Product, UpdateProductDTO } from '../../models/product.model';
+
+import { StoreService } from '../../services/store.service';
+import { ProductsService } from '../../services/products.service';
+import { da } from 'date-fns/locale';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent implements OnInit {
+
+  myShoppingCart: Product[] = [];
+  total = 0;
+  products: Product[] = [];
+  showProductDetail = false;
+  productChosen: Product = {
+    id: '',
+    price: 0,
+    images: [],
+    title: '',
+    category: {
+      id: '',
+      name: '',
+    },
+    description: ''
+  }
+  limit = 10
+  offset = 0
+
+  constructor(
+    private storeService: StoreService,
+    private productsService: ProductsService
+  ) {
+    this.myShoppingCart = this.storeService.getShoppingCart();
+  }
+
+  ngOnInit(): void {
+    this.productsService.getProductsByPage(10, 0)
+      .subscribe(data => {
+        this.products = data;
+      });
+  }
+
+  onAddToShoppingCart(product: Product) {
+    this.storeService.addProduct(product);
+    this.total = this.storeService.getTotal();
+  }
+
+  toggleProductDetail() {
+    this.showProductDetail = !this.showProductDetail;
+  }
+
+  onShowDetail(id: string) {
+    this.productsService.getProduct(id)
+      .subscribe(data => {
+        this.toggleProductDetail()
+        this.productChosen = data
+      })
+  }
+
+  createNewProduct() {
+    const product: CreateProductDTO = {
+      title: 'Nuevo producto',
+      price: 1000,
+      categoryId: 1,
+      description: 'Esta es la descripcion',
+      images: ['https://placeimg.com/640/480/any']
+    }
+
+    this.productsService.create(product)
+      .subscribe(data => {
+        this.products.unshift(data)
+      });
+  }
+
+  updateProduct() {
+    const changes: UpdateProductDTO = {
+      title: 'Nuevo titulo',
+    }
+
+    const id = this.productChosen.id;
+    this.productsService.update(id, changes)
+      .subscribe(data => {
+        const productIndex = this.products.findIndex(item => item.id == this.productChosen.id)
+        this.products[productIndex] = data;
+        this.productChosen = data;
+      })
+  }
+
+  deleteProduct() {
+    const id = this.productChosen.id;
+    this.productsService.delete(id)
+      .subscribe(() => {
+        const productIndex = this.products.findIndex(item => item.id == this.productChosen.id)
+        this.products.splice(productIndex, 1);
+        this.showProductDetail = false;
+      });
+  }
+
+  loadMore() {
+    this.productsService.getProductsByPage(this.limit, this.offset)
+      .subscribe(data => {
+        this.products = this.products.concat(data);
+        this.offset += this.limit;
+      });
+  }
+
+}
