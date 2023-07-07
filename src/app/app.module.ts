73a511dba2a36dd00a5501b54c5840544085d9b3
SantiagoOspinaO <santiago.m200@outlook.es>
2023-07-06 22:04:41 -0500

    Uso de interceptores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,41 @@
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { SwiperModule } from 'swiper/angular'
+import { BrowserModule } from '@angular/platform-browser';
+import { SwiperModule } from 'swiper/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImgComponent } from './components/img/img.component';
+import { NavComponent } from './components/nav/nav.component';
 import { ProductComponent } from './components/product/product.component';
 import { ProductsComponent } from './components/products/products.component';
-import { NavComponent } from './components/nav/nav.component';
+import { HighlightDirective } from './directives/highlight.directive';
 import { ReversePipe } from './pipes/reverse.pipe';
 import { TimeAgoPipe } from './pipes/time-ago.pipe';
-import { HighlightDirective } from './directives/highlight.directive';
+import { TimeInterceptor } from './interceptors/time.interceptor';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ImgComponent,
-    ProductComponent,
-    ProductsComponent,
-    NavComponent,
-    ReversePipe,
-    TimeAgoPipe,
-    HighlightDirective
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
-    SwiperModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ImgComponent,
+        ProductComponent,
+        ProductsComponent,
+        NavComponent,
+        ReversePipe,
+        TimeAgoPipe,
+        HighlightDirective
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule,
+        SwiperModule
+    ],
+    providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: TimeInterceptor, multi: true }
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
